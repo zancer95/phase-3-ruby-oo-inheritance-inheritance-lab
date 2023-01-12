27f73b1f096d50d72e839682272fd2897a98717b
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-12 10:28:42 -0500

    commit tree e5ad00e84d4ea39f7363551049f389ea4c2dc1d8

---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+
+    def first_name
+        @first_name
+    end
+    def first_name=(value)
+        @first_name = value
+    end
+    def last_name
+        @last_name
+    end
+    def last_name=(value)
+        @last_name = value
+    end
+
+end
